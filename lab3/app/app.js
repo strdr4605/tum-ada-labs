9847f56f6860a7c1f3fdb8e06ba243013eacf20c
Dragoș Străinu <str.dr4605@gmail.com>
2020-06-14 23:51:32 +0300

    Add docker-compose

--- lab3/app/app.js
+++ lab3/app/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 // New Code
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = monk('mongo:27017/nodetest1');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
